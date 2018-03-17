aec6aafecd1f53248949150c6d577b276fd45296
Alexandre Frigon <alexandre.frigon.1@gmail.com>
2018-03-17 02:37:48 -0400

    Flux radio preps

--- client/store/modules/index.js
+++ client/store/modules/index.js
@@ -1,9 +1,11 @@
 import Search from './search'
 import Temp from './temp'
+import Persistent from './persistent'
 
 const Modules = {
     search: Search,
-    temp: Temp
+    temp: Temp,
+    persistent: Persistent
 }
 
 export default Modules
