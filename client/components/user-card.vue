03581f9a30be96c3c6619379ba963c99d3264974
SamuelFoisy <samuel.foisy.1@ulaval.ca>
2018-04-19 13:25:42 -0400

    Friends page - first version

---
+++ client/components/user-card.vue
@@ -0,0 +1,36 @@
+<template lang="pug">
+    div
+        img.avatar.hoverable-pop.no-select.circle(:alt="`{{name}} Profile Picture`" :src="getAvatar")
+        .text-white {{name}}
+</template>
+
+<script>
+    import Api from '@/api'
+
+    export default {
+        props: {
+            name: String,
+            email: String
+        },
+        computed: {
+            getAvatar () {
+                return Api.getGravatar(150, this.email)
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+    .avatar
+    {
+        width: 75%;
+    }
+
+    @media only screen and (max-width: 600px) {
+        .avatar
+        {
+            width: 60%;
+        }
+    }
+</style>
--- client/pages/following.vue
+++ client/pages/following.vue
@@ -4,6 +4,8 @@
         loading(v-if="loading" color="#b29adb")
 
         .section
+            .absolute.action.action-left
+                button.text-button.transparent.text-primary-light(@click="back") Return
             .row
                 .column.s12(v-if="targetUser")
                     .column.s12.l3.text-center
@@ -11,10 +13,6 @@
                             img.hoverable-pop.no-select.circle(:alt="`${targetUser.name} Profile Picture`" :src="targetUser.avatar")
                             .row(v-if="targetUser.name")
                                 p.text-white.text-center.text-size-2.margin-0 {{ targetUser.name }}
-                            .row
-                                .text-white.clickable(@click="showFollowing")
-                                    i.material-icons list
-                                    span.icon-text {{ targetUser.following.length }} friend(s)
                             .row(v-if="targetUser.isFollowed")
                                 .text-white.clickable(@click="unfollow")
                                     i.material-icons remove_circle_outline
@@ -24,16 +22,20 @@
                                     i.material-icons person_add
                                     span.icon-text Follow
                     .column.s12.l9.padding-0
-                        show-playlist(:user-id="targetUser.id")
+                        .container.margin-up-30
+                            h1.text-white.text-size-3.text-light.text-center Friends ({{targetUser.following.length}})
+                            .section
+                                .row.text-center
+                                    user-card.user-card.clickable(v-for="followingUser in targetUser.following" :key="followingUser.id" :name="followingUser.name" :email="followingUser.email" @click.native="() => selectFollowingUser (followingUser.id)")
 
 </template>
 
 <script>
     // 5aaaad66d85c31000414d68a frigon
     // 5aac54dba6d6b600042f3082 Sam
-    import ShowPlaylist from '@/components/show-playlist'
     import ErrorBox from '@/components/error'
     import Loading from '@/components/loading'
+    import UserCard from '@/components/user-card'
 
     import Api, { UserApi } from '@/api'
 
@@ -41,7 +43,7 @@
         components: {
             'error': ErrorBox,
             'loading': Loading,
-            'show-playlist': ShowPlaylist
+            'user-card': UserCard
         },
         data: () => ({
             targetUser: null,
@@ -62,7 +64,7 @@
             try {
                 const user = await UserApi.getUser(to.params.id)
                 user.avatar = Api.getGravatar(150, user.email)
-                user.isFollowed = UserApi.isFollowing(user.id)
+                user.isFollowed = await UserApi.isFollowing(user.id)
                 return this.setData(user) & next()
             } catch (err) { return this.setData(null) & next() }
         },
@@ -71,15 +73,19 @@
             return next()
         },
         methods: {
+            back () {
+                return this.$router.go(-1)
+            },
             setData (data) {
                 this.loading = false
                 if (!data) {
                     this.targetUser = null
-                    return (this.error = 'An error occured while searching for this user.')
+                    return (this.error = 'An error occured while fetching data for this user.')
                 }
 
                 this.error = null
                 this.targetUser = data
+                console.log('targetUser', this.targetUser)
             },
             async follow () {
                 this.loading = true
@@ -99,13 +105,51 @@
                 } catch (err) { this.error = 'An error occured while following this user.' }
                 this.loading = false
             },
-            showFollowing () {
-                this.$router.push({ path: `/user/${this.targetUser.id}/friends` })
+            selectFollowingUser (id) {
+                this.$router.push({ path: `/user/${id}` })
             }
         }
     }
 </script>
 
 <style lang="scss" scoped>
+    .action {
+        top: 90px;
+        button {
+            padding: 5px 0;
+            text-align: center;
+            width: 75px;
+            font-weight: 200;
+            font-size: 1.32rem;
+        }
+        .text-button { text-shadow: 0px 0px 8px #555555 }
+        .full-button {
+            border-radius: 13px;
+            background-color: #484156;
+            padding: 0;
+            line-height: 31px;
+        }
+    }
+    .action-left { left: 32px; }
 
+    .user-card {
+        vertical-align: top;
+        display: inline-block;
+        margin: 15px;
+        width: 150px;
+        height: 150px;
+        margin-bottom: 40px;
+    }
+
+    .following-profile-image
+    {
+        width: 75%;
+    }
+
+    @media only screen and (max-width: 600px) {
+        .following-profile-image
+        {
+            width: 60%;
+        }
+    }
 </style>
